5e89fad1526d8d69944921bf9ebec81db8fa7db5
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2023-12-04 23:04:28 +0530

    Delivery updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Navbar from "./Components/Navbar";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./Components/style.css";
 import Hero from "./Components/Hero";
+import Delivery from "./Components/Delivery";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Delivery />
     </>
   );
 }
--- src/Components/Delivery.jsx
+++ src/Components/Delivery.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from "react";
+import Process1 from "../assets/Images/process1.png";
+import Process2 from "../assets/Images/process2.png";
+import Process3 from "../assets/Images/process3.png";
 
 const Delivery = () => {
   return (
-    <div>
-      
+    <div className="container px-4 py-5 delivery-process">
+      <h2 className="text-center display-4 mt-4 fw-bold">How Delivery Works</h2>
+      <div className="row g-4 py-5 row-cols-1 row-cols-lg-3">
+        <div className="col px-5 text-center">
+          <div className="mb-3">
+            <img src={Process1} alt="process1" className="w-50 h-50" />
+          </div>
+          <h2>Pick Your Veg</h2>
+          <p>
+            Veggies are a bonus for you, therefore I ask you to eat more
+            kohlrabi.
+          </p>
+        </div>
+        <div className="col px-5 text-center">
+          <div className="mb-3">
+            <img src={Process2} alt="process2" className="w-50 h-50" />
+          </div>
+          <h2>We Deliver the Bag</h2>
+          <p>
+            Gumbo, beet greens, corn, soko (possibly a type of vegetable),
+            endive, gumbo gourd, parsley.
+          </p>
+        </div>
+        <div className="col px-5 text-center">
+          <div className="mb-3">
+            <img src={Process3} alt="process3" className="w-50 h-50" />
+          </div>
+          <h2>You Enjoy Your Veg</h2>
+          <p>
+            Dandelion, cucumber, earthnut (possibly a type of tuber or legume),
+            pea, peanut, soko (a type of green leafy vegetable), zucchini.
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Delivery
+export default Delivery;
